6bf4fa89d3366baaf3963429d087b9f280b1130b
Bryan <breynolds1247@gmail.com>
2019-05-02 06:11:30 -0400

    updated efficiency point extraction to separate by jet collection

--- getEfficiencyPoints.py
+++ getEfficiencyPoints.py
@@ -3,17 +3,18 @@ import re
 import math
 import json
 import copy
+import pprint
 
 def main():
 
     x = ROOT.Double()
     #provide file path to root file containing turnon plots
-    path1 = "turnons_data17_withEmulation_02_26.root"
+    path1 = "turnons_2017_05_01.root"
     #path1 = "turnons.root"
     TFile1 = ROOT.TFile(path1)
 
     #name of output dictionary files (no file extension)
-    outputFilename = "efficiencyPointsDict_03_04"
+    outputFilename = "efficiencyPointsDict_2017_05_01"
 
     #specify efficiency point values, add more or less to the list as needed
     efficiencies = [0.95, 0.99, 0.995]
@@ -21,6 +22,10 @@ def main():
     efficiencyPointDict = {}
     efficiencyPointDictEmulated = {}
     efficiencyPointDictTDT = {}
+    efficiencyPointDictJVTEmulated = {}
+    efficiencyPointDictJVTTDT = {}
+    efficiencyPointDictPFlowEmulated = {}
+    efficiencyPointDictPFlowTDT = {}
     listHistNames = []
     listHists = []
 
@@ -42,14 +47,33 @@ def main():
         #isolate name of trigger from hist name
         histName = hist.GetName().rsplit('/', 1)[-1]
         efficiencyPoints = findEfficiencyPoints(hist, efficiencies)
-        if "Emulated" in hist.GetName():
-            histNameLabel = histName + "_Emulated"
-            efficiencyPointDictEmulated[histNameLabel] = efficiencyPoints
-            #print "'Emulated' found in: " + str(hist.GetName())
-        if "TDT" in hist.GetName():
-            histNameLabel = histName + "_TDT"
-            efficiencyPointDictTDT[histNameLabel] = efficiencyPoints
-            #print "'TDT' found in: " + str(hist.GetName())
+        if "smallR_JVT" in hist.GetName():
+            if "Emulated" in hist.GetName():
+                histNameLabel = histName + "_JVT_Emulated"
+                efficiencyPointDictJVTEmulated[histNameLabel] = efficiencyPoints
+                #print "'Emulated' found in: " + str(hist.GetName())
+            if "TDT" in hist.GetName():
+                histNameLabel = histName + "_JVT_TDT"
+                efficiencyPointDictJVTTDT[histNameLabel] = efficiencyPoints
+                #print "'TDT' found in: " + str(hist.GetName())
+        elif "smallR_PFlow_JVT" in hist.GetName():
+            if "Emulated" in hist.GetName():
+                histNameLabel = histName + "_PFlow_JVT_Emulated"
+                efficiencyPointDictPFlowEmulated[histNameLabel] = efficiencyPoints
+                #print "'Emulated' found in: " + str(hist.GetName())
+            if "TDT" in hist.GetName():
+                histNameLabel = histName + "_PFlow_JVT_TDT"
+                efficiencyPointDictPFlowTDT[histNameLabel] = efficiencyPoints
+                #print "'TDT' found in: " + str(hist.GetName())
+        elif ("JVT" not in hist.GetName()) and ("PFlow" not in hist.GetName()):
+            if "Emulated" in hist.GetName():
+                histNameLabel = histName + "_Emulated"
+                efficiencyPointDictEmulated[histNameLabel] = efficiencyPoints
+                #print "'Emulated' found in: " + str(hist.GetName())
+            if "TDT" in hist.GetName():
+                histNameLabel = histName + "_TDT"
+                efficiencyPointDictTDT[histNameLabel] = efficiencyPoints
+                #print "'TDT' found in: " + str(hist.GetName())
         #print(efficiencyPoints)
         efficiencyPointDict[histName] = efficiencyPoints
 
@@ -57,11 +81,20 @@ def main():
         #json.dump(efficiencyPointDict, outfile, indent=4)
         json.dump(efficiencyPointDictEmulated, outfile, indent=4)
         json.dump(efficiencyPointDictTDT, outfile, indent=4)
+        json.dump(efficiencyPointDictJVTEmulated, outfile, indent=4)
+        json.dump(efficiencyPointDictJVTTDT, outfile, indent=4)
+        json.dump(efficiencyPointDictPFlowEmulated, outfile, indent=4)
+        json.dump(efficiencyPointDictPFlowTDT, outfile, indent=4)
 
+    pp = pprint.PrettyPrinter(indent=1)
     with open(outputFilename+".py", "w") as filePy:
         #filePy.write(str(efficiencyPointDict))
-        filePy.write(str(efficiencyPointDictEmulated))
-        filePy.write(str(efficiencyPointDictTDT))
+        filePy.write(str(pp.pformat(efficiencyPointDictEmulated)))
+        filePy.write(str(pp.pformat(efficiencyPointDictTDT)))
+        filePy.write(str(pp.pformat(efficiencyPointDictJVTEmulated)))
+        filePy.write(str(pp.pformat(efficiencyPointDictJVTTDT)))
+        filePy.write(str(pp.pformat(efficiencyPointDictPFlowEmulated)))
+        filePy.write(str(pp.pformat(efficiencyPointDictPFlowTDT)))
     #listHists[0].Print("v")
 
 #function to find efficiency points for each turnon, specified efficiency value     
